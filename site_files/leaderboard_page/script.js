760ae8bbf46f951b0c95f6e209247bf1fe0411cd
veles <velesmyth@protonmail.com>
2019-10-10 00:03:18 -0500

    rough leaderboard page done

--- site_files/leaderboard_page/script.js
+++ site_files/leaderboard_page/script.js
@@ -1,17 +1,36 @@
-function showBoard() {
-	console.log("test");
-	$("#leader_text").css({"transition-duration": "0.5s"});
-	$("#leader_text").css({"opacity": 0});
+function showBoard(buttonNum) {
+	$("#leader_text").css({"display": "none"});
 	
-	var list = document.getElementById("top_lists");
+	$("#top_list1").css({"display": "none"});
+	$("#top_list2").css({"display": "none"});
+	$("#top_list3").css({"display": "none"});
 	
+	var list;
+	var nodeString;
+	switch(buttonNum) {
+		case 1: list = document.getElementById("top_list1");
+		nodeString = "test1";
+		$("#top_list1").css({"display": ""});
+		break;
+		case 2: list = document.getElementById("top_list2");
+		nodeString = "test2";
+		$("#top_list2").css({"display": ""});
+		break;
+		case 3: list = document.getElementById("top_list3");
+		nodeString = "test3";
+		$("#top_list3").css({"display": ""});
+		break;
+		default: return;
+	}
+	
+	if (list.innerText != "") {
+		return;
+	}
+	
+	/* forloop for later dynamic population of lists */
 	for (var i = 0; i < 10; i++) {
 		var entry = document.createElement("li");
-		entry.appendChild(document.createTextNode("test"));
+		entry.appendChild(document.createTextNode(nodeString));
 		list.appendChild(entry);
 	}
-
-	$("#clear_row").css({"display": ""});
-	$("#clear_row").css({"transition-duration": "0.5s"});
-	$("#clear_row").css({"opacity": 1});
 }
