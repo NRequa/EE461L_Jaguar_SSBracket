be545465470f66e71b9ce61bbbcb3c3164807f45
NRequa <nick.requa@gmail.com>
2019-10-11 12:43:28 -0500

    Put different stats tables onto one page, can swap view wthout going to new page.

---
+++ site_files/account_page/script.js
@@ -0,0 +1,44 @@
+function swapPage(pageName){
+    switch(pageName){
+        
+        // Tournament data wanted
+        case 0: {
+            $("#tournSect").show();
+            $("#settingsSect").hide();
+            $("#charSect").hide();
+            $("#friendsSect").hide();            
+            break;
+        }
+
+        // Char data wanted
+        case 1: {
+            $("#tournSect").hide();
+            $("#charSect").show();
+            $("#friendsSect").hide();
+            $("#settingsSect").hide();
+            break;
+        }
+
+        // friends data wanted
+        case 2: {
+            $("#tournSect").hide();
+            $("#charSect").hide();
+            $("#friendsSect").show();
+            $("#settingsSect").hide();
+            break;
+        }
+        // Settings page wanted
+        case 3: {
+            $("#tournSect").hide();
+            $("#charSect").hide();
+            $("#friendsSect").hide();
+            $("#settingsSect").show();
+            break;
+        }
+
+        // Log out requested
+        case 4: {
+
+        }
+    }
+}
