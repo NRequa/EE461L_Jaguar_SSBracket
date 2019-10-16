fc0e5b554671fc5b1477e31f58db2be0ac44cf44
Joseph Dieciedue <jdieciedue@utexas.edu>
2019-10-15 19:41:17 -0500

    Added fields to User model and removed remaining Google cloud info from properties.

--- apiREST/src/main/java/xyz/ssbracket/Model/User.java
+++ apiREST/src/main/java/xyz/ssbracket/Model/User.java
@@ -6,10 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -25,17 +22,18 @@ public class User implements Serializable {
     @Column(name = "username", nullable = false)
     private String username;
 
-    /*
-    @Column(name = "numWins", nullable = false)
-    private int numWins;
+    @Column(name = "num_wins", nullable = false)
+    private int num_wins;
 
-    @Column(name = "numTotalGames", nullable = false)
-    private int numTotalGames;
+    @Column(name = "num_games_played", nullable = false)
+    private int num_games_played;
 
-    @Column(name = "numTournamentsCreated", nullable = false)
-    private int numTournamentsCreated;
+    @Column(name = "num_tournaments_created", nullable = false)
+    private int num_tournaments_created;
 
-    @Column(name = "numTournamentsPlayedIn", nullable = false)
-    private int numTournamentsPlayedIn;
-    */
+    @Column(name = "num_tournaments_participated", nullable = false)
+    private int num_tournaments_participated;
+
+    @Column(name = "num_tournaments_won", nullable = false)
+    private int num_tournaments_won;
 }
