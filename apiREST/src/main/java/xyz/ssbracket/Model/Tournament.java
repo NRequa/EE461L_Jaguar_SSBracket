d33320d81454e2c67d3406f820101deff9bd39ed
Takuma <tfujiwara2017@gmail.com>
2019-10-20 11:00:02 -0500

    Many to many relationships work

--- apiREST/src/main/java/xyz/ssbracket/Model/Tournament.java
+++ apiREST/src/main/java/xyz/ssbracket/Model/Tournament.java
@@ -7,9 +7,16 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import java.io.Serializable;
 
+
+//new imports
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import java.util.HashSet;
+import java.util.Set;
+
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
+//@NoArgsConstructor
 @Entity
 @Table(name = "tournaments")
 public class Tournament implements Serializable {
@@ -29,4 +36,29 @@ public class Tournament implements Serializable {
 
 	@Column(name = "tsize", nullable = false)
 	private int tsize;
+
+
+
+//everything under this comment is new stuff
+  @ManyToMany(
+            fetch = FetchType.LAZY,
+            cascade = {CascadeType.PERSIST, CascadeType.MERGE}
+    )
+    @JoinTable(
+            name = "user_tournaments",
+            joinColumns = { @JoinColumn(name = "tournament_id") },
+            inverseJoinColumns = { @JoinColumn(name = "user_id") }
+    )
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Set<User> users = new HashSet<>();
+
+    public Tournament() {}
+
+    public Tournament(int id, String tname, String tcreator, int ttype, int tsize) {
+        this.id = id;
+        this.tname = tname;
+        this.tcreator = tcreator;
+        this.ttype = ttype;
+        this.tsize = tsize;
+    }
 }
