05e6c59be6e8d0595711a71bea9c693307260ce6
up1007 <tyler_takeyama@utexas.edu>
2019-10-25 13:15:01 -0500

    ONclick change People

--- site_files/bracket_page/script.js
+++ site_files/bracket_page/script.js
@@ -59,6 +59,11 @@ function loading(){
   bBox.appendChild(node);
 
 setName();//this enables setting the player's name
+
+$(document).ready(function(){
+  $('[data-toggle="tooltip"]').tooltip();
+});
+
 }
 
 function createPath(svg,midpoint1,midpoint2){
@@ -127,11 +132,40 @@ function rectClick(g_id){//set Editable
   console.log(g_id);
   var getG=document.getElementById(g_id);
   var cText=getG.childNodes[1];
+  var mod=document.getElementsByClassName("modal-title");
+  mod.innerHTML="Edit Player";
+  mod=document.getElementsByClassName("modal-text");
+  mod.innerHTML="Enter Player Name";
+  mod=document.getElementById("modal-ta");
+  mod.innerHTML=cText.innerHTML;
+  mod.setAttribute("rows","1");
+  mod.setAttribute("cols","50");
+  mod=document.getElementsByClassName("modal-data");
+  mod.innerHTML=""+g_id;
+  $("#myModal").modal();
+}
+function modalEnter(){
+  var mod=document.getElementsByClassName("modal-data");
+  var data=mod.innerHTML;
+  mod=document.getElementById("modal-ta");
+  setOneName(data,mod.value);
 
+}
+function setOneName(id,text){
+  //parsing for length of text
+  var arrG=document.getElementsByTagName('g');
+  arrG[id].childNodes[1].innerHTML=text;
 }
 function setName(){
   var arrG=document.getElementsByTagName('g');
   for(i=0;i<arrG.length;i++){
-    arrG[i].childNodes[1].innerHTML=i;
+    //cutNames
+    arrG[i].childNodes[1].innerHTML=i+"";
   }
+}
+function descClick(){
+
+}
+function titleClick(){
+
 }
