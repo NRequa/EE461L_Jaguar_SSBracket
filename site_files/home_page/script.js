2fb8d6130c4b62c5930879530e2a467f2d9da677
up1007 <tyler_takeyama@utexas.edu>
2019-10-25 23:08:41 -0500

    duplicate error

--- site_files/home_page/script.js
+++ site_files/home_page/script.js
@@ -3,18 +3,17 @@ toggleOn = false;
 $(document).ready(function() {
 	var Http = new XMLHttpRequest();
 	var url = "http://www.ssbracket.us-east-2.elasticbeanstalk.com/api/v1/tournament";
-	Http.open("GET", url);
-	Http.send();
-	
+
+
 	Http.onreadystatechange = (e) => {
 		var obj = JSON.parse(Http.responseText);
-		
+
 		var holder = [];
-		
+
 		var i;
 		var user;
 		var tmp;
-		
+
 		// 2D array holds player associated with win rate
 		for (i = 0; i < obj.data.content.length; i++) {
 			tmp = [];
@@ -24,26 +23,27 @@ $(document).ready(function() {
 			tmp.push(tourney.visits);
 			holder.push(tmp)
 		}
-		
+
+
 		console.log(holder);
-		
+
 		var j;
 		var n = holder.length;
-		
+
 		// insertion sort
-		for (i = 1; i < n; i++) {  
-			key = holder[i];  
-			j = i - 1;  
-  
-			while (j >= 0 && holder[j][2] > key[2]) {  
-				holder[j + 1] = holder[j];  
-				j = j - 1;  
-			}  
-			holder[j + 1] = key;  
+		for (i = 1; i < n; i++) {
+			key = holder[i];
+			j = i - 1;
+
+			while (j >= 0 && holder[j][2] > key[2]) {
+				holder[j + 1] = holder[j];
+				j = j - 1;
+			}
+			holder[j + 1] = key;
 		}
-		
+
 		var popular = document.getElementById("popular");
-		
+
 		var tname;
 		var description;
 		var visits;
@@ -52,21 +52,23 @@ $(document).ready(function() {
 		var entry;
 		for (i = 0; i < obj.data.content.length; i++) {
 			entry = document.createElement("div");
-			
+
 			tname = holder[obj.data.content.length - 1 - i][0];
 			description = holder[obj.data.content.length - 1 - i][1];
-			
+
 			header = document.createElement("h4")
 			header.innerHTML = tname;
 			entry.appendChild(header);
-			
+
 			para = document.createElement("p");
 			para.innerHTML = description;
 			entry.appendChild(para);
-			
+
 			popular.appendChild(entry);
 		}
 	}
+	Http.open("GET", url,true);
+	Http.send();
 });
 
 function showContactInfo() {
@@ -79,10 +81,10 @@ function populateDrop() {
 		$(".dropdown-toggle").dropdown("toggle");
 		toggleOn = false;
 	}
-	
+
 	var searchForm = document.getElementById("searchForm");
 	var searchTitle = document.getElementById("search_ph");
-	
+
 	if (event.keyCode == 13){
 		var text = searchForm.value;
 		if (text != "") {
--- site_files/leaderboard_page/script.js
+++ site_files/leaderboard_page/script.js
@@ -1,28 +1,28 @@
 $(document).ready(function() {
 	$("#content_row").css({"display": "none"});
-	
+
 	$("#content_row").css({"transition-duration": "0.5s"});
 	$("#content_row").css({"transform-origin": "top"});
 	$("#content_row").css({"transform": "scale(0)"});
-	
+
 	$("#btn1").click();
 });
 
 function showBoard(buttonNum) {
 	$("#content_row").css({"display": ""});
-	
+
 	resetAll(buttonNum);
-	
+
 	var list;
 	list = document.getElementById("top_list1");
 	$("#top_list1").css({"display": ""});
 	$("#btn1").css({"background-color": "rgb(230, 115, 0)"});
 	showTopUsers();
-	
+
 	if (list.innerText != "") {
 		return;
 	}
-	
+
 	$("#content_row").css({"transform": "scale(1)"});
 }
 
@@ -36,17 +36,15 @@ function showTopUsers() {
 	console.log("showing top users");
 	var Http = new XMLHttpRequest();
 	var url = "http://www.ssbracket.us-east-2.elasticbeanstalk.com/api/v1/user";
-	Http.open("GET", url);
-	Http.send();
-	
+
 	Http.onreadystatechange = (e) => {
 		var obj = JSON.parse(Http.responseText);
 		var holder = [];
-		
+
 		var i;
 		var user;
 		var tmp;
-		
+
 		// 2D array holds player associated with win rate
 		for (i = 0; i < obj.data.content.length; i++) {
 			tmp = [];
@@ -55,39 +53,41 @@ function showTopUsers() {
 			tmp.push((user.numgamesplayed / user.numwins).toFixed(2));
 			holder.push(tmp)
 		}
-		
+
 		console.log(holder);
-		
+
 		var j;
 		var n = holder.length;
-		
-		
+
+
 		// insertion sort
-		for (i = 1; i < n; i++) {  
-			key = holder[i];  
-			j = i - 1;  
-  
-			while (j >= 0 && holder[j][1] > key[1]) {  
-				holder[j + 1] = holder[j];  
-				j = j - 1;  
-			}  
-			holder[j + 1] = key;  
-		} 
-		
+		for (i = 1; i < n; i++) {
+			key = holder[i];
+			j = i - 1;
+
+			while (j >= 0 && holder[j][1] > key[1]) {
+				holder[j + 1] = holder[j];
+				j = j - 1;
+			}
+			holder[j + 1] = key;
+		}
+
 		$("#top_list1").empty();
 		var list = document.getElementById("top_list1");
-		
+
 		var name;
 		var val;
 		var entry;
 		for (i = 0; i < obj.data.content.length; i++) {
 			entry = document.createElement("li");
-			
+
 			name = holder[obj.data.content.length - 1 - i][0];
 			val = holder[obj.data.content.length - 1 - i][1];
-			
+
 			entry.appendChild(document.createTextNode(name + ": " + val));
 			list.appendChild(entry);
 		}
 	}
+	Http.open("GET", url);
+	Http.send();
 }
