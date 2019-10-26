6d6c1781f16c358d3e73dd08d35daf96112dd592
bsx-1 <velesmyth@protonmail.com>
2019-10-25 20:49:15 -0500

    copied scraper mocha test to tests folder

--- site_files/leaderboard_page/script.js
+++ site_files/leaderboard_page/script.js
@@ -4,6 +4,8 @@ $(document).ready(function() {
 	$("#content_row").css({"transition-duration": "0.5s"});
 	$("#content_row").css({"transform-origin": "top"});
 	$("#content_row").css({"transform": "scale(0)"});
+	
+	$("#btn1").click();
 });
 
 function showBoard(buttonNum) {
@@ -12,41 +14,13 @@ function showBoard(buttonNum) {
 	resetAll(buttonNum);
 	
 	var list;
-	var nodeString;
-	switch(buttonNum) {
-		case 1: list = document.getElementById("top_list1");
-		nodeString = "---- testOverall: Test Number ";
-		$("#top_list1").css({"display": ""});
-		$("#btn1").css({"background-color": "rgb(230, 115, 0)"});
-		break;
-		case 2: list = document.getElementById("top_list2");
-		nodeString = "---- testCharacter: Test Number ";
-		$("#top_list2").css({"display": ""});
-		$("#btn2").css({"background-color": "rgb(230, 115, 0)"});
-		break;
-		case 3: list = document.getElementById("top_list3");
-		nodeString = "---- testRegion: Test Number ";
-		$("#top_list3").css({"display": ""});
-		$("#btn3").css({"background-color": "rgb(230, 115, 0)"});
-		break;
-		default: return;
-	}
+	list = document.getElementById("top_list1");
+	$("#top_list1").css({"display": ""});
+	$("#btn1").css({"background-color": "rgb(230, 115, 0)"});
+	showTopUsers();
 	
 	if (list.innerText != "") {
 		return;
-	}
-	
-	/* forloop for later dynamic population of lists */
-	for (var i = 0; i < 20; i++) {
-		var testNumber = i + 1;
-		var entry = document.createElement("li");
-		entry.appendChild(document.createTextNode(nodeString + testNumber));
-		
-		var img = document.createElement("img");
-		img.src = getPicture(buttonNum);
-		entry.appendChild(img);
-		
-		list.appendChild(entry);
 	}
 	
 	$("#content_row").css({"transform": "scale(1)"});
@@ -54,25 +28,9 @@ function showBoard(buttonNum) {
 
 function resetAll() {
 	$("#top_list1").css({"display": "none"});
-	$("#top_list2").css({"display": "none"});
-	$("#top_list3").css({"display": "none"});
-	
 	$("#btn1").css({"background-color": "rgb(255, 191, 128)"});
-	$("#btn2").css({"background-color": "rgb(255, 191, 128)"});
-	$("#btn3").css({"background-color": "rgb(255, 191, 128)"});
 }
 
-function getPicture(buttonNum) {
-	switch(buttonNum) {
-		case(1): return "cat1.png";
-		break;
-		case(2): return "cat2.png";
-		break;
-		case(3): return "cat3.png";
-		break;
-		default: return null;
-	}
-}
 
 function showTopUsers() {
 	console.log("showing top users");
@@ -94,7 +52,7 @@ function showTopUsers() {
 			tmp = [];
 			user = obj.data.content[i];
 			tmp.push(user.username);
-			tmp.push(user.numgamesplayed / user.numwins);
+			tmp.push((user.numgamesplayed / user.numwins).toFixed(2));
 			holder.push(tmp)
 		}
 		
---
+++ tests/mochaTests/test.js
@@ -0,0 +1,92 @@
+var assert = require('assert');
+const get = require('./axios/lib/axios');
+
+
+// array to hold the json data. Possibly smelly, but making additional
+// promises to get around that is slow and prone to error
+var dataArray;
+
+describe('wiki tests', function() {
+	before(function() {
+		this.timeout(15000);
+		return new Promise((resolve) => {
+		    var fromBucket = get('http://www.ssbracket.xyz/scrape/data');
+	        fromBucket.then(function(data) {
+			//console.log(data);
+			//console.log(data.data);
+		    dataArray = data.data;
+		    resolve(dataArray);
+	        })
+	        .catch(function(err) {
+		        console.log(err)
+	        });	
+		});
+	});
+	describe('verify values exist', function() {
+		it('should return false for every defined weight', function() {
+			var i;
+			for(i = 0; i < dataArray.length; i++) {
+				assert.equal(typeof dataArray[i].tweight == undefined, false);
+			}
+		});
+		it('should return false for every defined dash, excepting Dr. Mario and Rosalina (see github issue)', function() {
+			var i;
+			for(i = 0; i < dataArray.length; i++) {
+				if(dataArray[i].name == 'Dr. Mario' || dataArray[i].name == 'Rosalina') continue;
+				assert.equal(typeof dataArray[i].tdash == undefined, false);
+			}
+		});
+		it('should return false for every defined spotdodge excepting Hero and Joker (see github issue)', function() {
+			var i;
+			for(i = 0; i < dataArray.length; i++) {
+				if(dataArray[i].name == 'Hero' || dataArray[i].name == 'Joker') continue;
+				assert.equal(typeof dataArray[i].tspotdodge == undefined, false);
+			}
+		});
+		it('should return false for every defined traction', function() {
+			var i;
+			for(i = 0; i < dataArray.length; i++) {
+				assert.equal(typeof dataArray[i].ttraction == undefined, false);
+			}
+		});
+    });
+	describe('verify values are reasonable', function() {
+		it('should return true for every reasonable weight', function() {
+			var i;
+			for(i = 0; i < dataArray.length; i++) {
+				//console.log(dataArray[i].name);
+				let truth = dataArray[i].tweight > 61 && dataArray[i].tweight < 136;
+				assert.equal(truth, true);
+			}
+		});
+		it('should return true for every reasonable dash, excepting Dr. Mario and Rosalina (see github issue)', function() {
+			var i;
+			for(i = 0; i < dataArray.length; i++) {
+				//console.log(dataArray[i].name);
+				if(dataArray[i].name == 'Dr. Mario' || dataArray[i].name == 'Rosalina') continue;
+				let truth = dataArray[i].tdash < 3.86 && dataArray[i].tdash > 1.17;
+				assert.equal(truth, true);
+			}
+		});
+		it('should return true for every reasonable spotdodge excepting Hero and Joker (see github issue)', function() {
+			var i;
+			for(i = 0; i < dataArray.length; i++) {
+				if(dataArray[i].name == 'Hero' || dataArray[i].name == 'Joker') continue;
+				let truth = dataArray[i].tspotdodge == '3-14'
+				         || dataArray[i].tspotdodge == '3-16'
+						 || dataArray[i].tspotdodge == '3-17'
+						 || dataArray[i].tspotdodge == '6-17'
+						 || dataArray[i].tspotdodge == '3-18';
+				assert.equal(truth, true);
+			}
+		});
+		it('should return true for every reasonable traction', function() {
+			var i;
+			for(i = 0; i < dataArray.length; i++) {
+				//console.log(dataArray[i].name);
+				let truth = dataArray[i].ttraction < .140 && dataArray[i].ttraction > .075;
+				assert.equal(truth, true);
+			}
+		});
+    });
+});
