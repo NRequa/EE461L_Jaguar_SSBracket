2ba56c98669236a7c06bf3a333ebd95dee00d5a4
Takuma <tfujiwara2017@gmail.com>
2019-11-13 17:09:28 -0600

    can search for tournaments by name

--- apiREST/src/main/java/xyz/ssbracket/Service/TournamentService.java
+++ apiREST/src/main/java/xyz/ssbracket/Service/TournamentService.java
@@ -2,8 +2,12 @@ package xyz.ssbracket.Service;
 
 import xyz.ssbracket.Model.Tournament;
 import xyz.ssbracket.Model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import java.util.List;
 
 public abstract class TournamentService implements MainService<Tournament> {
   public abstract Tournament addUsers( User o, int id );
   public abstract Tournament deleteUsers( User o, int id);
+  public abstract List<Tournament> searchTournamentName( Tournament tname );
 }
