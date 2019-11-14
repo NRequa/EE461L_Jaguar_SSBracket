c1c52ed08df1a550f8cc1d7b8eea91269e074fb1
NickDuggar <nbduggar@gmail.com>
2019-11-14 17:00:16 -0600

    Ready for AWS - fixed Dr Mario and Rosalina bugs, added error handling, and generally completed this part of the project

--- scraper/handler.js
+++ scraper/handler.js
@@ -5,15 +5,10 @@ const AWS = require('aws-sdk');
 
 AWS.config.update({
 	region: "us-east-2",
-	//endpoint: ""
 });
 
 // Run locally with: node -e "require('./handler').scrapeSSBWiki(null, {}, console.log)"
 
-const {extractWeightFromHTML} = require('./helpers');
-const {extractDashFromHTML} = require('./helpers');
-const {extractTractionFromHTML} = require('./helpers');
-const {extractSpotdodgeFromHTML} = require('./helpers');
 const {extractDataFromHTML} = require('./helpers');
 const {extractStringFromHTML} = require('./helpers');
 const {parseGames} = require('./helpers');
@@ -34,8 +29,6 @@ const {getTractionTraction} = require('./helpers');
 const {getSSBWorldCharURL} = require('./helpers');
 const {getSSBWorldWinLose} = require('./helpers');
 
-
-
 var characterData = [];
 
 module.exports.scrapeSSBWiki = (event, context, callback) => {
@@ -47,7 +40,7 @@ module.exports.scrapeSSBWiki = (event, context, callback) => {
   const characterSSBWorldNames = [];
   const characterSSBWorldPages = [];
   const characterGames = [];
-  //var jobs; // dummy variable to hold temp results
+
   // for the promises to synch calls
   var promiseWeight = get('https://www.ssbwiki.com/Weight');
   var promiseDash = get('https://www.ssbwiki.com/Dash');
@@ -56,48 +49,33 @@ module.exports.scrapeSSBWiki = (event, context, callback) => {
   var promiseSSBWorldChars = get('https://ssbworld.com/characters/');
 
   Promise.all([promiseWeight, promiseDash, promiseSpotdodge, promiseTraction, promiseSSBWorldChars]).then(function(data1) {
-	  //console.log("promise.log");
 	  extractDataFromHTML(data1[0].data, characterWeight, '.wikitable tbody tr', '.collapsed', [{name: "wtitle", fn: getWeightTitleCond}, {name: "wtitle2", fn: getWeightTitleCond}, {name: "name", fn: getWeightName}, {name: "weight", fn: getWeightWeight}], [{type: "notTypeof", value: "undefined"}, {type: "contains", value: 'SSBU'}]);
 	  extractDataFromHTML(data1[1].data, characterDash, '.wikitable tbody tr', '.collapsed', [{name: "wtitle", fn: getDashTitleCond}, {name: "wtitle2", fn: getDashTitleCond}, {name: "name", fn: getDashName}, {name: "dash", fn: getDashDash}], [{type: "notTypeof", value: "undefined"}, {type: "contains", value: 'SSBU'}]);
 	  extractDataFromHTML(data1[2].data, characterSpotdodge, '.wikitable tbody tr', '.collapsed', [{name: "wtitle", fn: getSpotdodgeTitleCond}, {name: "wtitle2", fn: getSpotdodgeTitleCond}, {name: "name", fn: getSpotdodgeName}, {name: "spotdodge", fn: getSpotdodgeSpotdodge}], [{type: "notTypeof", value: "undefined"}, {type: "contains", value: 'SSBU'}]);
 	  extractDataFromHTML(data1[3].data, characterTraction, '.wikitable tbody tr', '.collapsed', [{name: "wtitle", fn: getTractionTitleCond}, {name: "wtitle2", fn: getTractionTitleCond}, {name: "name", fn: getTractionName}, {name: "traction", fn: getTractionTraction}], [{type: "notTypeof", value: "undefined"}, {type: "contains", value: 'SSBU'}]);
-	  //console.log('extracted');
-	  //jobs = extractWeightFromHTML(data1[0].data, characterWeight);
-	  //jobs = extractDashFromHTML(data1[1].data, characterDash);
-	  //jobs = extractSpotdodgeFromHTML(data1[2].data, characterSpotdodge);
-	  //jobs = extractTractionFromHTML(data1[3].data, characterTraction);
 	  extractDataFromHTML(data1[4].data, characterSSBWorldURLs, '.players-list div a', null, [{name: "url", fn: getSSBWorldCharURL}], null);
-	  //characterData = combine(characterWeight, characterDash, characterSpotdodge, characterTraction);
 	  characterSSBWorldURLs.forEach(function(el) {
 		  characterSSBWorldPages.push(get(el.url));
 		  characterSSBWorldNames.push(parseURL(el.url));
-		  console.log(el);
 	  });
-	//console.log(characterData);
-	console.log(characterSSBWorldNames);
-	//console.log(JSON.stringify(characterData));
-	return Promise.all(characterSSBWorldPages); // maybe replace this with a promise all for the ssbworld pages
+	return Promise.all(characterSSBWorldPages);
   })
   .then(function(data) {
 	  data.forEach(function(el, i) {
 		  let winloss = extractStringFromHTML(el, "SSB World Game Record:", 6, "<").trim();
-		  //let name = extractStringFromHTML(el, "<h1 class=\\\"bio\\\">", 0, "<").trim();// TODO
-		  console.log("returned");
-		  console.log(winloss);
 		  let games = parseGames(winloss);
 		  games.name = characterSSBWorldNames[i];
 		  characterGames.push(games);
 	  });
-	  console.log(characterGames);
-      return Promise.resolve(characterGames); // replace later with something leading to combining the data into one JSON
+      return Promise.resolve(characterGames);
   })
   .then(function(data) {
       characterData = combine(characterWeight, characterDash, characterSpotdodge, characterTraction, characterGames);
       return Promise.resolve(characterData);
   })
-//  .then(function(data) {
-//	  return putIntoS3('www.ssbracket.xyz'/*process.env.bucketname*/, 'scrape/data', JSON.stringify(data))
-//	  })
+  .then(function(data) {
+	  return putIntoS3('www.ssbracket.xyz'/*process.env.bucketname*/, 'scrape/data', JSON.stringify(data))
+  })
   .then(function(data) {
 	  callback(null, {data})
   })
@@ -146,28 +124,56 @@ function combine(weight, dash, spotdodge, traction, games) {
 	    	    break;
 	        }
 		}
-		combined.push({name, tweight, tdash, tspotdodge, ttraction, ttotalGames, twins, tlosses});
+		// Error handling due to differing naming conventions across sites
+		switch(name) {
+			case "Charizard":
+			case "Ivysaur":
+			case "Squirtle":
+			    for(j = 0; j < games.length; j++) {
+	                if(games[j].name === "Pokemon Trainer") {
+	    	            ttotalGames = games[j].total;
+				        twins = games[j].wins;
+				        tlosses = games[j].losses;
+						break;
+					}
+	            }
+				break;
+		    case "Dr. Mario":
+			    for(j = 0; j < dash.length; j++) {
+	                if(dash[j].name === "Mario") {
+	    	            tdash = dash[j].dash;
+	    	            break;
+	                }
+				}
+				break;
+			case "Rosalina":
+			    for(j = 0; j < dash.length; j++) {
+	                if(dash[j].name === "Rosalina & Luma") {
+	    	            tdash = dash[j].dash;
+	    	            break;
+	                }
+				}
+				break;
+			case "Bayonetta":
+			    tspotdodge = tspotdodge.substr(0, 4);
+			default: // no error!
+			    break;
 		}
+		// If all else fails
+	    if(typeof tweight === "undefined") tweight = "No data";
+		if(typeof tdash === "undefined") tdash = "No data";
+		if(typeof tspotdodge === "undefined") tspotdodge = "No data";
+		if(typeof ttraction === "undefined") ttraction = "No data";
+		if(typeof ttotalGames === "undefined") ttotalGames = "No data";
+		if(typeof twins === "undefined") twins = "No data";
+		if(typeof tlosses === "undefined") tlosses = "No data";
+		combined.push({name, tweight, tdash, tspotdodge, ttraction, ttotalGames, twins, tlosses});
+	}
 	return combined;
 }
 
 function putIntoS3(bucket, key, data) {
 	var s3 = new AWS.S3();
-	//console.log('s31');
-	//console.log(data);
-	//console.log('s32');
-	
-	var old;
-	/*var getParams = {
-		Bucket: bucket,
-		Key: key
-	};
-	s3.deleteObject(getParams, function(err, data) {
-		if (err) console.log(err, err.stack); // an error occurred
-        else     {
-			console.log(data);           // successful response
-		}
-	});*/
     var params = {
         Bucket : bucket,
         Key : key,
--- scraper/helpers.js
+++ scraper/helpers.js
@@ -64,69 +64,10 @@ function getSSBWorldWinLose(row) {
 	return row.root().children().eq(0).children().eq(1).children().eq(1).children().eq(2).children().eq(5).children().eq(0).attr('class');
 }
 
-//extractDataFromHTML(html, characterData, '.wikitable tbody tr', '.collapsed', [getWeightTitleCond, getWeightTitleCond, getWeightName, getWeightWeight], []);
-
-function extractWeightFromHTML (html, characterData) {
-  const $ = cheerio.load(html);
-  const characterRows = $('.wikitable tbody tr').not('.collapsed');
-  characterRows.each((i, el) => {
-	if(typeof $(el).children().eq(1).children().last().attr('title') === "undefined") {return;}
-	if($(el).children().eq(1).children().last().attr('title').toString().includes('SSBU') === false) {return;}
-    let name = $(el).children().eq(1).children().last().text().trim();
-    let weight = $(el).children().eq(2).text().trim();
-
-    characterData.push({name, weight});
-  });
-
-  return characterData;
-}
-
-function extractDashFromHTML (html, characterData) {
-  const $ = cheerio.load(html);
-  const characterRows = $('.wikitable tbody tr').not('.collapsed');
-  characterRows.each((i, el) => {
-	if(typeof $(el).children().eq(1).children().last().attr('title') === "undefined") {return;}
-	if($(el).children().eq(1).children().last().attr('title').toString().includes('SSBU') === false) {return;}
-    let name = $(el).children().eq(1).children().last().text().trim();
-    let dash = $(el).children().eq(3).text().trim();
-	characterData.push({name, dash});
-  });
-
-  return characterData;
-}
-
-function extractSpotdodgeFromHTML (html, characterData) {
-  const $ = cheerio.load(html);
-  const characterRows = $('.wikitable tbody tr').not('.collapsed');
-  characterRows.each((i, el) => {
-	if(typeof $(el).children().eq(0).children().last().attr('title') === "undefined") {return;}
-	if($(el).children().eq(0).children().last().attr('title').toString().includes('SSBU') === false) {return;}
-    let name = $(el).children().eq(0).children().last().text().trim();
-    let spotdodge = $(el).children().eq(1).text().trim();
-	characterData.push({name, spotdodge});
-  });
-
-  return characterData;
-}
-
-function extractTractionFromHTML (html, characterData) {
-  const $ = cheerio.load(html);
-  const characterRows = $('.wikitable tbody tr').not('.collapsed');
-  characterRows.each((i, el) => {
-	if(typeof $(el).children().eq(1).children().last().attr('title') === "undefined") {return;}
-	if($(el).children().eq(1).children().last().attr('title').toString().includes('SSBU') === false) {return;}
-    let name = $(el).children().eq(1).children().last().text().trim();
-    let traction = $(el).children().eq(2).text().trim();
-	characterData.push({name, traction});
-  });
-
-  return characterData;
-}
-
 /*
     html is the html document from axios that we want to scrape
 	array is an input array that we want to add to
-	rowsDefinition is optional. If you want to iterate over regular elements, put in class and tag names for the elements
+	rowsDefinition is to iterate over regular elements; put in class and tag names for the elements
 	rowsNotDefition requires rowsDefinition, and is optional. Use if you want to preclude elements from iteration.
 	
 	elementSelectors are functions that define what elements you want to select from the HTML. 
@@ -137,160 +78,67 @@ function extractTractionFromHTML (html, characterData) {
 	    will be tested against the elements selected - elements selected must == the conditions to be included. Else,
 		they will be skipped. Elements with conditionals are not included in the return. 
 		
-	Output depends on iteration. If iterating, output is an array of objects, with each object containing key:value pairs
+	Output is an array of objects, with each object containing key:value pairs
 	If you aren't using some optional values, input null!
 */
 function extractDataFromHTML (html, array, rowsDefinition, rowsNotDefinition, elementSelectors, elementConditionals) {
-	//console.log("extracting data from html. These are the arguments:");
-	//console.log(html);
-	//console.log("the array:");
-	//console.log(array);
-	//console.log(rowsDefinition);
-	//console.log(rowsNotDefinition);
-	//console.log("element selectors");
-	//console.log(elementSelectors);
-	//console.log("element conditionals");
-	//console.log(elementConditionals);
 	const $ = cheerio.load(html);
 	var rows;
 	var iterating = false;
 	if(rowsDefinition != null) {
-		//console.log("iterating");
 		iterating = true;
 		if(rowsNotDefinition != null) {
-			//console.log("row exclusion found");
 			rows = $(rowsDefinition).not(rowsNotDefinition);
 		}
 		else {
-			//console.log("no row exclusion found");
 			rows = $(rowsDefinition);
 		}
-		//console.log("the rows:");
-		//console.log(rows);
 	}
 	else {
+		console.error("null rowsDefinition - cannot work!");
+		return array;
 		rows = null;
-		//console.log("rows = null");
 	}
 	if(iterating) {
-		//console.log('starting iteration');
 		rows.each((i, el) => {
-			//console.log("iterate");
-			//console.log(i);
-			//console.log(el);
 			let selected = [];
 			elementSelectors.forEach((sel, i) => {
-				//console.log('sel');
-				//console.log(sel);
-				//console.log(el);
 				selected.push({name: sel.name, prop: sel.fn($(el))});
 			});
 			if(elementConditionals != null && elementConditionals != 'undefined') {
-				//console.log("element conditionals detected");
 				let success = true;
 				elementConditionals.forEach((con) => {
 					let thing = selected.shift();
-					//console.log(thing);
 					thing = thing.prop;
-					//console.log(thing);
 					switch(con.type) {
 						case "typeof":
 						    if(typeof thing != con.value) {success = false;}
-							//console.log(success);
 							break;
 						case "notTypeof":
 						    if(typeof thing == con.value) {success = false;}
-							//console.log(success);
 							break;
 						case "equal":
 						    if(thing != con.value) {success = false;}
-							//console.log(success);
 							break;
 						case "notEqual":
 						    if(thing == con.value) {success = false;}
-							//console.log(success);
 							break;
 						case "contains":
 						    if(success == false) break;
 						    if(!thing.toString().includes(con.value)) {success = false;}
-							//console.log(success);
 							break;
 						default: success = false;
 					}
 			    });
-				//console.log("after condition testing");
-			    //console.log(success);
 				if(!success) {return;}
 			}	
 			let object = {};
 			selected.forEach((prop) => {
-				//console.log(prop);
 				object[prop.name] = prop.prop;
 			});
-			//console.log(object);
 			array.push(object);
 		});
 	}
-	/*else { // TODO: edit this to match above
-	    console.log("not iterating");
-		let selected = [];
-		elementSelectors.forEach((sel) => {
-			//let fn = sel.fn;
-			//let elem = $(fn);
-			//let thingToPush = {name: sel.name, prop: sel.fn($)};
-			//console.log(cheerio.html($(thingToPush.prop)));
-			selected.push({name: sel.name, prop: sel.fn($)});
-			
-		});
-		console.log('the selected elements');
-		//console.log(selected[0]);
-		if(elementConditionals != null && elementConditionals != 'undefined') {
-			console.log("element conditionals detected");
-			let success = true;
-			elementConditionals.forEach((con) => {
-				let thing = selected.shift();
-				//console.log(thing);
-				thing = thing.prop;
-				//console.log(thing);
-			    switch(con.type) {
-						case "typeof":
-						    if(typeof thing != con.value) {success = false;}
-							//console.log(success);
-							break;
-						case "notTypeof":
-						    if(typeof thing == con.value) {success = false;}
-							//console.log(success);
-							break;
-						case "equal":
-						    if(thing != con.value) {success = false;}
-							//console.log(success);
-							break;
-						case "notEqual":
-						    if(thing == con.value) {success = false;}
-							//console.log(success);
-							break;
-						case "contains":
-						    if(success == false) break;
-						    if(!thing.toString().includes(con.value)) {success = false;}
-							//console.log(success);
-							break;
-						default: success = false;
-					}
-		    });
-			console.log("after condition testing");
-			console.log(success);
-			if(!success) {return array;} // object isn't good, so return the array.
-		}	
-		let object = {};
-		selected.forEach((prop) => {
-			console.log(prop);
-			object[prop.name] = prop.prop;
-		});
-		console.log(object);
-		array.push(object);
-	}*/
-	//console.log("the array");
-	//console.log(array);
 	return array;
 }
 
@@ -318,6 +166,7 @@ function extractStringFromHTML(html, keyPhrase, skip, end) {
 }
 
 function parseGames(winloss) {
+	winloss = winloss.replace(/,/g, "");
 	let wins = "";
 	let reading = 0;
 	while(!(winloss.charAt(reading) === " ")) {
@@ -338,16 +187,11 @@ function parseGames(winloss) {
 }
 
 function parseURL(url) {
-	console.log("parseUrl: ".concat(url));
 	let trimmed = url.substr(32).trim(); // remove the https://ssbworld.com/characters/ part
-	console.log("trimmed: ".concat(trimmed));
 	let output = "";
-	let letters = "qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM";
 	let prevChar = " ";
 	for(let i = 0; i < trimmed.length; i++) {
-		console.log(output.charCodeAt(i));
 		if(trimmed.charCodeAt(i) === 45) { 
-		    console.log("DASH DETECTED");
 		    output = output.concat(" ");
 			prevChar = " ";
 		}
@@ -369,7 +213,6 @@ function parseURL(url) {
 	else if(output === "Banjo And Kazooie") output = "Banjo & Kazooie";
 	else if(output === "Pac Man") output = "Pac-Man";
 	else if(output === "Rosalina And Luma") output = "Rosalina";
-	console.log("output: ".concat(output));
 	return output;
 }
 
@@ -388,10 +231,6 @@ module.exports = {
   getTractionTraction,
   getSSBWorldCharURL,
   getSSBWorldWinLose,
-  extractWeightFromHTML,
-  extractDashFromHTML,
-  extractSpotdodgeFromHTML,
-  extractTractionFromHTML,
   extractDataFromHTML,
   extractStringFromHTML,
   parseGames,
