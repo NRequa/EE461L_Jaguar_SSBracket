73649e125b6b0bc5ce95ad714ab855a91463be72
Joseph Dieciedue <jdieciedue@utexas.edu>
2019-11-14 15:59:07 -0600

    Can change name of users in match with mapping "/setp1string/{id}" and "/setp2string/{id}".

--- apiREST/src/main/java/xyz/ssbracket/Service/MatchService.java
+++ apiREST/src/main/java/xyz/ssbracket/Service/MatchService.java
@@ -6,4 +6,6 @@ import xyz.ssbracket.Model.MatchResult;
 public abstract class MatchService implements MainService<MatchResult> {
     public abstract MatchResult updateUsers( MatchResult o, int id );
     public abstract MatchResult addGuestUser( MatchResult o );
+    public abstract MatchResult updateP1String( MatchResult o, int id);
+    public abstract MatchResult updateP2String( MatchResult o, int id);
 }
