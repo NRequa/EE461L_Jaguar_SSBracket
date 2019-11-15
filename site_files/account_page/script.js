3c2351ed9e3912a6382fd43c969ee87189ec8300
NRequa <nick.requa@gmail.com>
2019-11-15 12:52:14 -0600

    Fixed log out button, restrict whitespace in username.

--- site_files/account_page/script.js
+++ site_files/account_page/script.js
@@ -44,7 +44,8 @@ function swapPage(pageName){
 
         // Log out requested
         case 4: {
-
+            sessionStorage.clear;
+            document.location.href = "loginPrompt.html";
         }
     }
 }
@@ -239,9 +240,37 @@ function addBuddy(){
     xmlhttp.send(JSON.stringify(formData));
 }
 
+function killFriend(){
+        var apiCall = 'http://ssbracket.us-east-2.elasticbeanstalk.com/api/v1/user/deletefriend/' + sessionStorage.getItem("userId");
+        
+        var formData = {
+            "username" : $('input[id=palUserName]').val(),
+        }
+    
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function(){
+            console.log(this.readyState);
+            console.log(this.status);
+            console.log(this.responseText);
+            if(this.readyState == 4 && this.status == 200){
+                alert("Friend removed :(");
+            }
+    
+            else if(this.readyState == 4 && this.status == 400){
+                alert("That user isn't your friend");
+            }
+        };
+    
+        xmlhttp.open("PATCH",apiCall);
+        xmlhttp.setRequestHeader("Content-Type", "application/json");
+       // xmlhttp.setRequestHeader("Accept", "*/*");
+        xmlhttp.send(JSON.stringify(formData));
+    }
+
+
 function changePictureRequest(imageName){
   var myuserid = sessionStorage.getItem("userId");
-  var apiCall = 'http://www.ssbracket.us-east-2.elasticbeanstalk.com/api/v1/user/updateavatar/'+myuserid;
+  var apiCall = 'http://ssbracket.us-east-2.elasticbeanstalk.com/api/v1/user/updateavatar/'+myuserid;
   var formData = {
       "avatarName" : imageName
   }
@@ -264,7 +293,7 @@ function changePictureRequest(imageName){
 
 function resetPassword(){
     var myuserid = sessionStorage.getItem("userId");
-    var apiCall = 'http://www.ssbracket.us-east-2.elasticbeanstalk.com/api/v1/Accounts/updatePass/'+myuserid;
+    var apiCall = 'http://ssbracket.us-east-2.elasticbeanstalk.com/api/v1/Accounts/updatePass/'+myuserid;
     var formData = {
         "username" :  "bigDummy",
         "password" :  $('input[id=regPass]').val()
