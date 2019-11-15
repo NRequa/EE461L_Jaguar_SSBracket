7ddcc71be84298d99573aaf4c1d53ee1d6e1aa97
Nick Requa <nick.requa@gmail.com>
2019-11-15 00:35:54 -0600

    Added updatePassword PATCH request to AccountsController

--- apiREST/src/main/java/xyz/ssbracket/Controller/AccountsController.java
+++ apiREST/src/main/java/xyz/ssbracket/Controller/AccountsController.java
@@ -1,8 +1,13 @@
 package xyz.ssbracket.Controller;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Pattern;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +21,12 @@ import xyz.ssbracket.Model.LogInResult;
 import xyz.ssbracket.Model.AccountSubmission;
 import xyz.ssbracket.Repository.AccountsRepository;
 import xyz.ssbracket.Repository.UserRepository;
+import static xyz.ssbracket.Constants.ApiConstants.MESSAGE_FOR_REGEX_NUMBER_MISMATCH;
+import static xyz.ssbracket.Constants.ApiConstants.REGEX_FOR_NUMBERS;
+
+import xyz.ssbracket.Results.ResponseWrapper;
+import xyz.ssbracket.Service.AccountService;
+
 
 @Validated
 @RestController
@@ -27,6 +38,8 @@ public class AccountsController{
     AccountsRepository accountsLog;
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    private AccountService accountMainService;
 
     // Login check
    // @PostMapping("/login")
@@ -110,6 +123,16 @@ public class AccountsController{
 
 
         }
+    }
+
+
+
+    @CrossOrigin
+    @PatchMapping(value = "/updatePass/{id}")
+    public ResponseWrapper<Integer> updatePassword(@Valid @RequestBody AccountSubmission userId,
+                                               @Valid @Pattern(regexp = REGEX_FOR_NUMBERS, message = MESSAGE_FOR_REGEX_NUMBER_MISMATCH) @PathVariable(value = "id") String id )
+    {
+        return new ResponseWrapper<>( accountMainService.updatePassword(Integer.parseInt(id), userId.getPassword()), HttpStatus.OK );
     }
 
 }
