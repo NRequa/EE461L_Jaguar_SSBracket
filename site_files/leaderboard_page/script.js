4b5dd7b20cda36ac833b7f5a9505654a8228e2e8
NRequa <nick.requa@gmail.com>
2019-12-02 14:16:24 -0600

    Added factory pattern to leaderboard page.

--- site_files/leaderboard_page/script.js
+++ site_files/leaderboard_page/script.js
@@ -36,8 +36,9 @@ function showTopUsers() {
 	console.log("showing top users");
 	var Http = new XMLHttpRequest();
 	var url = "http://www.ssbracket.us-east-2.elasticbeanstalk.com/api/v1/user";
+	var cmd = "";
 
-	Http.onreadystatechange = (e) => {
+	var readyFunc = function() {
 		
 		var obj = JSON.parse(Http.responseText);
 		var holder = [];
@@ -101,6 +102,71 @@ function showTopUsers() {
 			list.appendChild(entry);
 		}
 	}
+
+	/*
 	Http.open("GET", url);
 	Http.send();
+	*/
+	var requestMaker = new RequestFactory();
+	var request = requestMaker.createRequest("user", cmd, readyFunc);
+	request.httpObject.open("GET", request.callURL);
+	request.httpObject.send();
+}
+
+function RequestFactory() {
+    this.createRequest = function(type, command, func){
+        var request;
+
+        if(type == "accounts"){
+            request = new AccountsRequest(command);
+        }
+
+        else if (type == "matchresult"){
+            request = new MatchResultRequest(command);
+        }
+
+        else if (type == "tournament"){
+            request = new TournamentRequest(command);
+        }
+        else if (type == "user"){
+            request = new UserRequest(command);
+        }
+
+        else if (type == "scrape"){
+            request = new ScrapeRequest(command);
+        }
+
+        request.type = type;
+        request.command = command;
+        request.recallFunction = func;
+
+        request.httpObject = new XMLHttpRequest();
+        request.httpObject.onreadystatechange = func;
+
+        return request;
+    }
+}
+
+
+
+var AccountsRequest = function(command) {
+        this.callURL = "http://www.ssbracket.us-east-2.elasticbeanstalk.com/api/v1/Accounts/" + command;
+}
+
+var MatchResultRequest = function(command) {
+    this.callURL = "http://www.ssbracket.us-east-2.elasticbeanstalk.com/api/v1/match/" + command;
+}
+
+var TournamentRequest = function(command) {
+    this.callURL = "http://www.ssbracket.us-east-2.elasticbeanstalk.com/api/v1/tournament/" + command;
+
+}
+
+var UserRequest = function(command) {
+    this.callURL = "http://www.ssbracket.us-east-2.elasticbeanstalk.com/api/v1/user/" + command;
+
+}
+
+var ScrapeRequest  = function(command) {
+    this.callURL = "../../scrape/" + command;
 }
